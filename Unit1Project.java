403737d13d345f554fd22e07ec2f3f6e32af27ef
BassAJ22 <71032384+BassAJ22@users.noreply.github.com>
2020-09-30 21:44:27 -0400

    Add files via upload

---
+++ Unit1Project.java
@@ -0,0 +1,37 @@
+package practice;
+
+public class Unit1Project{
+	public static void main(String[] args){
+		
+		String FirstName ="Annabel";
+		char ab=FirstName.charAt(0);
+		
+		String MiddleName="Jean";
+		char cd=MiddleName.charAt(0);
+		
+		String LastName="Bass";
+		char ef=LastName.charAt(0);
+		char wx=LastName.charAt(1);
+		char yz=LastName.charAt(2);
+		
+		String Birthday="04-12-2004";
+		char gh=Birthday.charAt(0);
+		char ij=Birthday.charAt(1);
+		char kl=Birthday.charAt(3);
+		char mn=Birthday.charAt(4);
+		char op=Birthday.charAt(6);
+		char qr=Birthday.charAt(7);
+		char st=Birthday.charAt(8);
+		char uv=Birthday.charAt(9);
+	
+		
+	System.out.print("Email:"+ef+wx+yz+ab+cd);
+	System.out.println("@sharkmail.com");
+	System.out.println("Temporary Password:" + gh+ij+kl+mn+op+qr+st+uv);
+	System.out.println("***This Temporary Password is very insecure! Please change it immediatly!!");
+	
+		
+		
+	}
+
+}
